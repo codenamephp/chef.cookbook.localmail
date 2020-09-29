b239820381d52cc76473ac3d2d42816ae1736d50
Bastian Schwarz <bastian@codename-php.de>
2020-09-29 23:30:50 +0200

    Maybe overlay2?

--- resources/mailhog.rb
+++ resources/mailhog.rb
@@ -22,7 +22,7 @@ action :install do
     repo 'mailhog/mailhog'
     port ['1025:1025', "#{new_resource.webui_port}:8025"]
     restart_policy 'always'
-    volume_driver 'vfs'
+    volume_driver 'overlay2'
   end
 
   package 'remove sendmail package' do
